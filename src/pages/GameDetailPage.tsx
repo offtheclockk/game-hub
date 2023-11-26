0c238caf30c2fb844f0f77fb1ea6ac1bc9800d1f
offtheclockk <88730624+offtheclockk@users.noreply.github.com>
2023-11-26 13:52:45 -0600

    Build game attributes

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,23 +1,27 @@
-import { Heading, Spinner, Text } from '@chakra-ui/react';
-import React from 'react'
-import { useParams } from 'react-router-dom';
-import ExpandableText from '../components/ExpandableText';
-import useGame from '../hooks/useGame';
+import { Heading, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
+import React from "react";
+import { useParams } from "react-router-dom";
+import CriticScore from "../components/CriticScore";
+import DefinitionItem from "../components/DefinitionItem";
+import ExpandableText from "../components/ExpandableText";
+import GameAttributes from "../components/GameAttributes";
+import useGame from "../hooks/useGame";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
   const { data: game, isLoading, error } = useGame(slug!);
-  
+
   if (isLoading) return <Spinner />;
 
   if (error || !game) throw error;
-  
+
   return (
     <>
       <Heading>{game.name}</Heading>
-      <ExpandableText>{ game.description_raw}</ExpandableText>
+      <ExpandableText>{game.description_raw}</ExpandableText>
+      <GameAttributes game={ game} />
     </>
-  )
-}
+  );
+};
 
-export default GameDetailPage
+export default GameDetailPage;
